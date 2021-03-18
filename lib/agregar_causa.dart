95aabe43443e34c104d2daeecf338546a56b37a3
Hernan <preguntasapp@gmajcenter.org>
2021-03-18 20:46:41 -0300

    sacamos el card

--- lib/agregar_causa.dart
+++ lib/agregar_causa.dart
@@ -21,86 +21,82 @@ class _AgregarCausaState extends State<AgregarCausa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Card(
-          margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
-          elevation: 2,
-          child: _cargando
-              ? Expanded(child: Center(child: CircularProgressIndicator()))
-              : Container(
-                  padding: const EdgeInsets.all(30),
-                  child: Form(
-                      key: _formKey,
-                      child: ListView(children: <Widget>[
-                        TextFormField(
-                          decoration:
-                              InputDecoration(helperText: 'Ingrese titulo'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Ingrese un titulo';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            _titulo = value;
-                          },
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(helperText: 'Ingrese subtitulo'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Ingrese un subtitulo';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            _subtitulo = value;
-                          },
-                        ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                              helperText: 'Ingrese descripcion'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Ingrese un descripcion';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            _descripcion = value;
-                          },
-                        ),
-                        TextFormField(
-                          decoration:
-                              InputDecoration(helperText: 'Ingrese objetivo'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Ingrese un objetivo';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            _objetivo = value;
-                          },
-                        ),
-                        TextFormField(
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(RegExp('[0-9]')),
-                          ],
-                          decoration:
-                              InputDecoration(helperText: 'Ingrese contacto'),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Ingrese un contacto';
-                            }
-                            return null;
-                          },
-                          onChanged: (value) {
-                            _contacto = int.parse(value);
-                          },
-                        ),
-                      ])),
-                )),
+      body: _cargando
+          ? Center(child: CircularProgressIndicator())
+          : Container(
+              padding: const EdgeInsets.all(30),
+              child: Form(
+                  key: _formKey,
+                  child: ListView(children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(helperText: 'Ingrese titulo'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Ingrese un titulo';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _titulo = value;
+                      },
+                    ),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(helperText: 'Ingrese subtitulo'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Ingrese un subtitulo';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _subtitulo = value;
+                      },
+                    ),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(helperText: 'Ingrese descripcion'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Ingrese un descripcion';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _descripcion = value;
+                      },
+                    ),
+                    TextFormField(
+                      decoration:
+                          InputDecoration(helperText: 'Ingrese objetivo'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Ingrese un objetivo';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _objetivo = value;
+                      },
+                    ),
+                    TextFormField(
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp('[0-9]')),
+                      ],
+                      decoration:
+                          InputDecoration(helperText: 'Ingrese contacto'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Ingrese un contacto';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        _contacto = int.parse(value);
+                      },
+                    ),
+                  ])),
+            ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
           FocusScope.of(context).unfocus();
