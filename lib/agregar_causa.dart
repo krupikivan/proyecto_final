ec16839459b4cc49b564240febf9d55851468927
Ivan Krupik <krupikivan@gmail.com>
2021-03-18 23:40:48 -0300

    loading

--- lib/agregar_causa.dart
+++ lib/agregar_causa.dart
@@ -17,94 +17,106 @@ class _AgregarCausaState extends State<AgregarCausa> {
   String _descripcion;
   String _objetivo;
   int _contacto;
+  bool _cargando = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Card(
           margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 15),
           elevation: 2,
-          child: Container(
-            padding: const EdgeInsets.all(30),
-            child: Form(
-                key: _formKey,
-                child: ListView(children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(helperText: 'Ingrese titulo'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Ingrese un titulo';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _titulo = value;
-                    },
-                  ),
-                  TextFormField(
-                    decoration:
-                        InputDecoration(helperText: 'Ingrese subtitulo'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Ingrese un subtitulo';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _subtitulo = value;
-                    },
-                  ),
-                  TextFormField(
-                    decoration:
-                        InputDecoration(helperText: 'Ingrese descripcion'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Ingrese un descripcion';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _descripcion = value;
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(helperText: 'Ingrese objetivo'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Ingrese un objetivo';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _objetivo = value;
-                    },
-                  ),
-                  TextFormField(
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(RegExp('[0-9]')),
-                    ],
-                    decoration: InputDecoration(helperText: 'Ingrese contacto'),
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Ingrese un contacto';
-                      }
-                      return null;
-                    },
-                    onChanged: (value) {
-                      _contacto = int.parse(value);
-                    },
-                  ),
-                ])),
-          )),
+          child: _cargando
+              ? CircularProgressIndicator()
+              : Container(
+                  padding: const EdgeInsets.all(30),
+                  child: Form(
+                      key: _formKey,
+                      child: ListView(children: <Widget>[
+                        TextFormField(
+                          decoration:
+                              InputDecoration(helperText: 'Ingrese titulo'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Ingrese un titulo';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _titulo = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration:
+                              InputDecoration(helperText: 'Ingrese subtitulo'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Ingrese un subtitulo';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _subtitulo = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              helperText: 'Ingrese descripcion'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Ingrese un descripcion';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _descripcion = value;
+                          },
+                        ),
+                        TextFormField(
+                          decoration:
+                              InputDecoration(helperText: 'Ingrese objetivo'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Ingrese un objetivo';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _objetivo = value;
+                          },
+                        ),
+                        TextFormField(
+                          inputFormatters: [
+                            FilteringTextInputFormatter.allow(RegExp('[0-9]')),
+                          ],
+                          decoration:
+                              InputDecoration(helperText: 'Ingrese contacto'),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Ingrese un contacto';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            _contacto = int.parse(value);
+                          },
+                        ),
+                      ])),
+                )),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
           FocusScope.of(context).unfocus();
           if (_formKey.currentState.validate()) {
+            setState(() {
+              _cargando = true;
+            });
             if (await agregarCausa(
                 titulo: _titulo,
                 descripcion: _descripcion,
                 contacto: _contacto,
                 objetivo: _objetivo,
                 subtitulo: _subtitulo)) {
+              setState(() {
+                _cargando = false;
+              });
               showDialog(
                   context: context,
                   child: AlertDialog(
@@ -119,6 +131,9 @@ class _AgregarCausaState extends State<AgregarCausa> {
                     ],
                   ));
             } else {
+              setState(() {
+                _cargando = false;
+              });
               showDialog(
                   context: context,
                   child: AlertDialog(
